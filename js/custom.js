0e3237931808fda539fd111b33b9e01fa2c106ba
trwinowiecki <trw0511@gmail.com>
2018-12-11 22:57:32 -0500

    portfolio beginning

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,47 @@
-$( document ).ready(function() {
-    
-    $("body").scrollspy({
-        target: "#navbar",
-        offset: 75
-    });
-    
+$(document).ready(function() {
+
+  $("body").scrollspy({
+    target: "#navbar",
+    offset: 75
+  });
+
+  $('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').trigger('focus')
+  })
+
+  $('.nav-side-items li').each(function() {
+    if ($('#' + this.id + ':has(a.active)').length) {
+      $('#' + this.id + ':has(a.active)').addClass('active');
+    } else {
+      $('#' + this.id).removeClass('active');
+    }
+  });
+
+  $(window).on('activate.bs.scrollspy', function() {
     $('.nav-side-items li').each(function() {
-        if ($('#' + this.id + ':has(a.active)').length) {
-            $('#' + this.id + ':has(a.active)').addClass('active');
-        } else {
-            $('#' + this.id).removeClass('active');
-        }
+      if ($('#' + this.id + ':has(a.active)').length) {
+        $('#' + this.id + ':has(a.active)').addClass('active');
+      } else {
+        $('#' + this.id).removeClass('active');
+      }
     });
-    
-    $(window).on('activate.bs.scrollspy', function () {
-        $('.nav-side-items li').each(function() {
-            if ($('#' + this.id + ':has(a.active)').length) {
-                $('#' + this.id + ':has(a.active)').addClass('active');
-            } else {
-                $('#' + this.id).removeClass('active');
-            }
-        });
-    });
-    
-    $('.nav-side-collapse').on('click', function () {
-       $('.nav-left-side').toggleClass('expanded');
-       $('.content-wrapper').toggleClass('expanded');
-    });
-    
-    
-    $('.nav-side-items ul a').hover(
-        function () {
-            $(this.hash + '-nav-text a').addClass('nav-side-hover');
-            $(this.hash + '-nav-icon a').addClass('nav-side-hover');
-        }, function () {
-            $(this.hash + '-nav-text a').removeClass('nav-side-hover');
-            $(this.hash + '-nav-icon a').removeClass('nav-side-hover');
-        }
-    );
-    
+  });
+
+  $('.nav-side-collapse').on('click', function() {
+    $('.nav-left-side').toggleClass('expanded');
+    $('.content-wrapper').toggleClass('expanded');
+  });
+
+
+  $('.nav-side-items ul a').hover(
+    function() {
+      $(this.hash + '-nav-text a').addClass('nav-side-hover');
+      $(this.hash + '-nav-icon a').addClass('nav-side-hover');
+    },
+    function() {
+      $(this.hash + '-nav-text a').removeClass('nav-side-hover');
+      $(this.hash + '-nav-icon a').removeClass('nav-side-hover');
+    }
+  );
+
 });
